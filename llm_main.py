8b246c6115f940c0efcedf428da5df09938e5c9a
VaibhavSachdeva26 <vaibhavsachdeva026@outlook.com>
2024-09-18 16:17:00 +0530

    Update llm_main.py

--- llm_main.py
+++ llm_main.py
@@ -7,21 +7,19 @@ from Processing.llm_processing import get_resume_score
 st.title("LLM-based Resume Sorting")
 
 job_desc = st.text_area("Enter Job Description")
-uploaded_files = st.file_uploader("Upload Resumes", accept_multiple_files=True, type=['pdf', 'doc', 'docx'])
-
+uploaded_files = st.file_uploader("Upload Resumes", accept_multiple_files=True, type=['pdf', 'doc', 'docx','pptx'])
 
 if st.button("Process Resumes"):
     resume_texts = [extract_text(file) for file in uploaded_files]
     table = []
     for resume in resume_texts:
-       scores= get_resume_score(job_desc, resume)
-       
-       table.append(str(scores))
+        scores = get_resume_score(job_desc, resume)
+        table.append(str(scores))
 
-    data = [json.loads(item) for item in table]   
+    data = [json.loads(item) for item in table]
     df = pd.DataFrame(data)
-    st.dataframe(df[['Name', 'Skills', 'Certification', 'Rating']]) 
+    sorted_df = df.sort_values(by='Rating', ascending=False)
+    st.dataframe(sorted_df[['Name', 'Skills', 'Certification', 'Rating']])
 
-    
 
-    
+
