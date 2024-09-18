9a24bd160ab9df5dcad3e63a2a43197e0c755f94
VaibhavSachdeva26 <vaibhavsachdeva026@outlook.com>
2024-09-18 16:48:27 +0530

    Update resume_processing.py

--- Preprocessing/resume_processing.py
+++ Preprocessing/resume_processing.py
@@ -1,12 +1,15 @@
 from PyPDF2 import PdfReader
 import docx
-
+from pptx import Presentation
+import os
 
 def extract_text(file):
     if file.type == "application/pdf":
         return extract_pdf_text(file)
     elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
         return extract_docx_text(file)
+    elif file.type == "application/vnd.openxmlformats-officedocument.presentationml.presentation":
+        return extract_ppt_text(file)
 
 
 def extract_pdf_text(file):
@@ -25,3 +28,22 @@ def extract_pdf_text(file):
 def extract_docx_text(file):
     doc = docx.Document(file)
     return "\n".join([para.text for para in doc.paragraphs])
+
+def extract_ppt_text(file):
+    # Load the presentation
+    prs = Presentation(file)
+
+    # Initialize a list to store the text
+    text_runs = []
+
+    # Iterate through each slide in the presentation
+    for slide in prs.slides:
+        # Iterate through each shape in the slide
+        for shape in slide.shapes:
+            # Check if the shape has text
+            if hasattr(shape, "text"):
+                text_runs.append(shape.text)
+    ppt_text = ''
+    for text in text_runs:
+        ppt_text = ppt_text + os.linesep + text
+    return ppt_text
